8b91275b47c69d2a593c0902f38140986e8fffdd
Moaz <mkhalifa@damlag.com>
2020-04-07 22:49:05 +0200

    init

---
+++ app/controllers/spree/api/v1/markets_controller.rb
@@ -0,0 +1,16 @@
+module Spree
+	module Api
+    module V1
+			class MarketsController < Spree::Api::BaseController
+				def index
+					ip = Rails.env.production? request.remote_ip ? : Net::HTTP.get(URI.parse('http://checkip.amazonaws.com/')).squish
+					coordinates = Geocoder.coordinates(ip)
+					dist = 5000
+					type = 'grocery_or_supermarket'
+					markets = Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV['GOOGLE_API_KEY']}&location=#{coordinates.join(',')}&radius=#{dist}&type=#{type}"))
+					render json: JSON.parse(markets)['results']
+				end
+			end
+		end
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # This line mounts Spree's routes at the root of your application.
+  # This means, any requests to URLs such as /products, will go to
+  # Spree::ProductsController.
+  # If you would like to change where this engine is mounted, simply change the
+  # :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Spree relies on it being
+  # the default of "spree".
+  mount Spree::Core::Engine, at: '/'
+	Spree::Core::Engine.add_routes do
+    namespace :api, defaults: { format: 'json' } do
+      namespace :v1 do
+        # Our new route goes here!
+        resources :markets, only: [:index]
+      end
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
