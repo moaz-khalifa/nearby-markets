b860e45d77840aeb13b92cd156d50e9896654c3d
Moaz <mkhalifa@damlag.com>
2020-04-09 02:17:12 +0200

    find markets in 5 km not 50

--- app/controllers/spree/api/v1/markets_controller.rb
+++ app/controllers/spree/api/v1/markets_controller.rb
@@ -5,7 +5,7 @@ module Spree
 				def index
 					ip = Rails.env.production? ? request.remote_ip : Net::HTTP.get(URI.parse('http://checkip.amazonaws.com/')).squish
 					coordinates = Geocoder.coordinates(ip)
-					markets = Spree::Market.near(coordinates, 50, units: :km)
+					markets = Spree::Market.near(coordinates, 5, units: :km)
 					render json: markets
 				end
 			end
